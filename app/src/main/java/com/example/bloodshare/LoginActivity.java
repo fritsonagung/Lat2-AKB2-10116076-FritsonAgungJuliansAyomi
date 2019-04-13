ceea24dc73f9ed01bbf9bd870bef5c108a3d9446
fritsonagung <agungfritson@gmail.com>
2019-04-13 14:42:27 +0700

    .

--- app/src/main/java/com/example/bloodshare/LoginActivity.java
+++ app/src/main/java/com/example/bloodshare/LoginActivity.java
@@ -2,6 +2,9 @@ package com.example.bloodshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+import android.content.Intent;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,9 +15,24 @@ public class LoginActivity extends AppCompatActivity {
     Tanggal Buat : 13 April 2019
  */
 
+    /*
+    CHANGE LOG :
+    1. Sabtu 13 April 2019 10.30, Membuat Layout Login Activity
+    2. Sabtu 13 April 2019 13.30, Membuat Fungsi Perpindahan dari Login Activity Ke Register Activity
+    3. Sabtu 13 April 2019 13.45, Membuat Layout Register Activity
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView register = findViewById(R.id.registerTX);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
