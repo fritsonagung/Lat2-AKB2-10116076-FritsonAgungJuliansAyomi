aa6ac5ebe35e92413511c428063d08c02fa00794
fritsonagung <agungfritson@gmail.com>
2019-04-13 12:44:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bloodshare/VerifyAccountActivity.java
@@ -0,0 +1,20 @@
+package com.example.bloodshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerifyAccountActivity extends AppCompatActivity {
+
+    /*
+    Nama         : Fritson Agung Julians Ayomi
+    NIM          : 10116076
+    Kelas        : AKB-2 / IF-2
+    Tanggal Buat : 13 April 2019
+ */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_account);
+    }
+}
