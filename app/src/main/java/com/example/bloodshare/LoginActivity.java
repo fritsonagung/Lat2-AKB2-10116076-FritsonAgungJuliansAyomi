5277af6726f5d9ca4af6dabd07fee0b1a8c68e06
fritsonagung <agungfritson@gmail.com>
2019-04-14 11:47:51 +0700

    Semua Layout Selesai

--- app/src/main/java/com/example/bloodshare/LoginActivity.java
+++ app/src/main/java/com/example/bloodshare/LoginActivity.java
@@ -22,9 +22,11 @@ public class LoginActivity extends AppCompatActivity {
     3. Sabtu 13 April 2019 13.45, Membuat Layout Register Activity
     4. Sabtu 13 April 2019 16.12, Membuat Fungsi Perpindahan dari Register Activity Ke Almost There Activity
     5. Minggu 14 April 2019 06.30, Membuat Layout Almost There Activity
-    6. Minggu 14 April 2019 06.56, Membuat Fungsi Perpindahan dari Almost There Activity Ke Verify Account Actifity
+    6. Minggu 14 April 2019 06.56, Membuat Fungsi Perpindahan dari Almost There Activity Ke Verify Account Activity
     7. Minggu 14 April 2019 07.00, Membuat Layout Custom Spinner
     8. Minggu 14 April 2019 07,45, Mebuat Layout Verify Account Activity
+    9  Minggu 14 April 2019 08.03, Membuat Fungsi Perpindahan dari Verify Account Activity Ke User Home Activity
+    8. Minggu 14 April 2019 10,22, Membuat Layout User Home Activity
      */
 
     @Override
